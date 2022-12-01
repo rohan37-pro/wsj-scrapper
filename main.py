d627984f3382ba1dc56dc1af8f7e148a6a2d60b3
rohan37-pro <rohan2002maji@gmail.com>
2022-12-01 15:11:38 +0530

    store the scrapping data

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
+import requests
 import json
 import sys
-import pyexcel
+import os
+import openpyxl
 from time import sleep
+
+working_directory = os.getcwd()
 
 def credentials():
 	with open('credentials.txt','r') as file:
@@ -45,7 +49,6 @@ def login():
 
 
 
-
 def main():
 	try:
 		if sys.argv[1] == 'login':
@@ -77,34 +80,81 @@ def main():
 		article = stories.get_attribute("href")
 		article_list.append(article)
 
-		print(f"author {author}")
-		print(f"date {date}")
-		print(f"image {image}")
-		print(f"article {article}")
-		print(f"head_line {head_line}")
-		print()	
+		print(f"author : {author}")
+		print(f"date : {date}")
+		print(f"image : {image}")
+		print(f"article : {article}")
+		print(f"head_line : {head_line}")
+		print()
+
+		# image = requests.get(image)
+		all_data_of_articles.append([head_line, author, date, image])
 
 
-	add_para = ''
+
+	i = 1
 	for article in article_list:
+		add_para = ''
 		driver.get(article)
 		sleep(2)
 
+		head_line = all_data_of_articles[i][0]
+		author = all_data_of_articles[i][1]
+		date = all_data_of_articles[i][2]
+		image = driver.find_element('xpath', "(//div[@class='paywall css-1u1nl00-PaywalledContentContainer e1db8bjv0']//img)[1]")
+		image_link = image.get_attribute('srcset')
+		image_link = image_link.split(" ")
+		image_link = image_link[-2]
+		print(f"image_link ------->    {image_link}")
+		image = requests.get(image_link)
+		image = image.content
+		# video = driver.find_element('xpath', "(//div[@class='video-wrapper player-8U']//video)[1]")
+		# video.a
 		paragraphs = driver.find_elements('xpath', "//section//p[@data-type='paragraph']")
 
 		for p in paragraphs:
 			add_para += p.text + '\n'
 
 		print(add_para)
-		print()
-		print()
-		print()
-		print()
-		print()
-		print()
-	
+		all_data_of_articles[i].append(add_para)
+
+		add_para = f'''{head_line}\n\n\n{author}\n{date}\n\n\n\n\n{add_para}'''
+		if not os.path.exists(working_directory + f'/articles/{head_line}'):
+			os.mkdir(working_directory + f'/articles/{head_line}')
+		with open(working_directory + f"/articles/{head_line}/{head_line}.txt", 'w') as file:
+			file.write(add_para)
+
+		with open(working_directory + f"/articles/{head_line}/{head_line}.jpg", 'wb') as file:
+			file.write(image)
+
+		i += 1
+
+	wb = openpyxl.load_workbook('article.xlsx')
+	sheet = wb.active
+	row = sheet.max_row
+	column = sheet.max_column
+	for i in all_data_of_articles:
+		sheet.append(i)
+	wb.save('article.xlsx')
+
 
 if __name__ == "__main__":
+	if not os.path.exists(working_directory + '/article.xlsx'):
+		all_data_of_articles = [['head_line', 'author', 'date', 'image', 'article']]
+		wb = openpyxl.Workbook()
+		sheet = wb.active
+		for i in range(1, 6):
+			cell_ = sheet.cell(row=1, column=i)
+			cell_.value = all_data_of_articles[0][i-1]
+		wb.save(working_directory + "/article.xlsx")
+	else:
+		all_data_of_articles = [[]]
+
+	if not os.path.exists(working_directory + "/articles/"):
+		print("file is not present")
+		os.mkdir(working_directory + "/articles")
+
+
 	main()
 
 	
